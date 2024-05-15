a4a96d5e54f165ea5bdd6d7f7db70acd104fa6f4
Mr-Maglor <159529274+Mr-Maglor@users.noreply.github.com>
2024-05-15 19:06:26 +0200

    Add files via upload

---
+++ Ressources/Groupe_auto_Users.ps1
@@ -0,0 +1,43 @@
+﻿#######################################
+#                                     #
+#   Création de groupes automatique   #
+#                                     #
+#######################################
+
+$FilePath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
+
+$File = "$FilePath\Groupe_User.csv"
+
+$Groups = Import-Csv -Path $File -Delimiter "," -Header "OU","SousOU","Designation"
+$ADGroup = Get-ADGroup -Filter * -Properties *
+$DomainDN = (Get-ADDomain).DistinguishedName
+
+foreach ($Group in $Groups) 
+{  
+        # Gestion de présence de Sous OU
+        if ( $Group.SousOU -eq "NA" )
+        # Chemin complet
+        { 
+                $Path = "OU=$($Group.OU),OU=Computer_Pharmgreen,DC=pharmgreen,DC=org"
+        }
+        Else
+        # Chemin sans sous OU
+        { 
+                $Path ="OU=$($Group.SousOU),OU=$($Group.OU),OU=Computer_Pharmgreen,DC=pharmgreen,DC=org"      
+        }
+        
+        # Ajout automatique des groupes
+        If (($ADGroup | Where {$_.Name -eq $Group.Designation}) -eq $Null)
+               {
+                        Try {
+                        New-AdGroup -Name $Group.Designation -Path $Path -GroupScope Global -GroupCategory Security
+                       Write-Host "Création du groupe $($Group.Designation) dans l'OU $($Group.OU)/$($Group.SousOU), $DomainDN réussie" -ForegroundColor Green
+                       }
+                       Catch 
+                       { write-host "Création du groupe $($Group.Designation) dans l'OU $($Group.OU)/$($Group.SousOU), $DomainDN échoué" -ForegroundColor red}
+                       }
+        Else
+                {
+                Write-Host "Le groupe $($Group.Designation) dans l'OU $($Group.OU)/$($Group.SousOU), $DomainDN  existe déjà" -ForegroundColor Black -BackgroundColor Yellow
+                }
+}
