e300e4a968207ba0a00fc9883d68c0c8c770a232
Kamalors <162970946+Kamalors@users.noreply.github.com>
2024-05-15 16:15:29 +0200

    Update GROUP_Creation_auto(1).ps1

--- Ressources/GROUP_Creation_auto(1).ps1
+++ Ressources/GROUP_Creation_auto(1).ps1
@@ -8,13 +8,13 @@ $FilePath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definitio
 
 $File = "$FilePath\Nomenclature.csv"
 
-$Groups = Import-Csv -Path $File -Delimiter ";" -Header "OU","SousOU","Designation"
+$Groups = Import-Csv -Path $File -Delimiter "," -Header "OU","SousOU","Designation"
 
 $DomainDN = (Get-ADDomain).DistinguishedName
 
 foreach ($Group in $Groups) {
     Try {
-        New-AdGroup -Name $($Group.Designation) -Path "OU=$($Group.SousOU), OU=$($Group.OU),OU=User_Pharmgreen,DC=pharmgreen,DC=org $DomainDN" -GroupScope Global -GroupCategory Security
+        New-AdGroup -Name $($Group.Designation) -Path "OU=$($Group.SousOU), OU=$($Group.OU),OU=User_Pharmgreen,DC=pharmgreen,DC=org" -GroupScope Global -GroupCategory Security
         Write-Host "Création du groupe $($Group.Designation) dans l'OU $($Group.OU)/$($Group.SousOU), $DomainDN réussie" -ForegroundColor Green
     } Catch {
         Write-Host "Le groupe $($Group.Designation) existe déjà" -ForegroundColor Yellow -BackgroundColor Black
