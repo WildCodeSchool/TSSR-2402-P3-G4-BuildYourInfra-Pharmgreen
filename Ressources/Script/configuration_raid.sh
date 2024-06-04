17816d3cdea6421a938dce914a24d6e2b45d0bda
Kamalors <162970946+Kamalors@users.noreply.github.com>
2024-06-04 12:37:23 +0200

    Update configuration_raid.sh

--- Ressources/Script/configuration_raid.sh
+++ Ressources/Script/configuration_raid.sh
@@ -24,7 +24,7 @@ echo "Entrez le disque cible (par exemple, /dev/sdb) :"
 read target_disk
 
 # Cloner la table de partition
-sudo sfdisk -d $source_disk | sudo sfdisk $target_disk
+sfdisk -d $source_disk | sfdisk $target_disk
 check_success "Clonage de la table de partition"
 pause
 
@@ -47,13 +47,13 @@ echo "Entrez le numéro de la partition cible pour RAID 1 (par exemple, 3) :"
 read target_part2
 
 # Installation de MDADM
-sudo apt-get install mdadm
+apt-get install mdadm
 check_success "Installation de MDADM"
-sudo cat /proc/mdstat
+cat /proc/mdstat
 pause
 
 # Modifier le type de partition
-sudo fdisk $target_disk <<EOF
+fdisk $target_disk <<EOF
 t
 $target_part1
 42
@@ -66,68 +66,68 @@ check_success "Modification du type de partition"
 pause
 
 # Création du RAID 1 en mode dégradé
-sudo mdadm --create /dev/md0 --level=1 --raid-disks=2 --metadata=1.0 missing $target_disk$target_part1
+mdadm --create /dev/md0 --level=1 --raid-disks=2 --metadata=1.0 missing $target_disk$target_part1
 check_success "Création du RAID 1 sur /dev/md0"
-sudo mdadm --create /dev/md1 --level=1 --raid-disks=2 --metadata=1.0 missing $target_disk$target_part2
+mdadm --create /dev/md1 --level=1 --raid-disks=2 --metadata=1.0 missing $target_disk$target_part2
 check_success "Création du RAID 1 sur /dev/md1"
 pause
 
 # Reconfiguration de MDADM
-sudo dpkg-reconfigure mdadm
+dpkg-reconfigure mdadm
 check_success "Reconfiguration de MDADM"
-sudo cp /etc/mdadm/mdadm.conf /etc/mdadm/mdadm.conf-dist
-sudo mdadm --examine --scan | sudo tee -a /etc/mdadm/mdadm.conf
+cp /etc/mdadm/mdadm.conf /etc/mdadm/mdadm.conf-dist
+mdadm --examine --scan | tee -a /etc/mdadm/mdadm.conf
 check_success "Mise à jour de mdadm.conf"
-sudo nano /etc/mdadm/mdadm.conf
+nano /etc/mdadm/mdadm.conf
 echo "Ajoutez DEVICE $target_disk$target_part1 $target_disk$target_part2 à mdadm.conf, puis appuyez sur Entrée pour continuer..."
 read
 
 # Formatage des partitions sur le disque N°2
-sudo mkfs.vfat /dev/md0
+mkfs.vfat /dev/md0
 check_success "Formatage de /dev/md0"
-sudo mkfs.ext4 /dev/md1
+mkfs.ext4 /dev/md1
 check_success "Formatage de /dev/md1"
 pause
 
 # Montage des partitions du disque N°2
-sudo mkdir /mnt/md0 && sudo mkdir /mnt/md1
+mkdir /mnt/md0 && mkdir /mnt/md1
 check_success "Création des points de montage"
-sudo mount /dev/md0 /mnt/md0 && sudo mount /dev/md1 /mnt/md1
+mount /dev/md0 /mnt/md0 && mount /dev/md1 /mnt/md1
 check_success "Montage des partitions"
 pause
 
 # Modification du FSTAB
-sudo cp /etc/fstab /etc/fstab.bak
+cp /etc/fstab /etc/fstab.bak
 check_success "Sauvegarde de /etc/fstab"
-sudo nano /etc/fstab
-sudo systemctl daemon-reload
+nano /etc/fstab
+systemctl daemon-reload
 check_success "Rechargement du démon systemd"
 echo "Modifiez /etc/fstab si nécessaire, puis appuyez sur Entrée pour continuer..."
 read
 
 # Mise à jour de GRUB
-sudo nano /etc/default/grub
-sudo update-grub
+nano /etc/default/grub
+update-grub
 check_success "Mise à jour de GRUB"
-sudo dpkg-reconfigure grub-pc
+dpkg-reconfigure grub-pc
 check_success "Reconfiguration de GRUB"
 echo "Modifiez /etc/default/grub si nécessaire, puis appuyez sur Entrée pour continuer..."
 read
 
 # Copie des données sur le disque N°2
-sudo rsync -auHxv --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* /* /mnt/md1/
+rsync -auHxv --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* /* /mnt/md1/
 check_success "Copie des données vers /mnt/md1"
-sudo rsync -auHxv /boot/efi /mnt/md0
+rsync -auHxv /boot/efi /mnt/md0
 check_success "Copie des données de /boot/efi vers /mnt/md0"
 pause
 
 # Mise à jour de GRUB et Initramfs sur le disque N°2
-sudo cp /etc/mdadm/mdadm.conf /mnt/md1/etc/mdadm/mdadm.conf
+cp /etc/mdadm/mdadm.conf /mnt/md1/etc/mdadm/mdadm.conf
 check_success "Copie de mdadm.conf vers /mnt/md1"
-sudo mount --bind /dev /mnt/md1/dev
-sudo mount --bind /proc /mnt/md1/proc
-sudo mount --bind /sys /mnt/md1/sys
-sudo chroot /mnt/md1
+mount --bind /dev /mnt/md1/dev
+mount --bind /proc /mnt/md1/proc
+mount --bind /sys /mnt/md1/sys
+chroot /mnt/md1
 check_success "Chroot vers /mnt/md1"
 update-initramfs -u
 check_success "Mise à jour d'initramfs"
@@ -137,14 +137,14 @@ exit
 pause
 
 # Mise à jour de GRUB sur le disque N°1
-sudo cp /boot/grub/grub.cfg /boot/grub/grub.cfg.bak
+cp /boot/grub/grub.cfg /boot/grub/grub.cfg.bak
 check_success "Sauvegarde de /boot/grub/grub.cfg"
-sudo cp /mnt/md1/boot/grub/grub.cfg /boot/grub/grub.cfg
+cp /mnt/md1/boot/grub/grub.cfg /boot/grub/grub.cfg
 check_success "Copie de grub.cfg vers /boot/grub/grub.cfg"
 pause
 
 # Modification de la partition sur le disque N°1
-sudo fdisk $source_disk <<EOF
+fdisk $source_disk <<EOF
 t
 $source_part1
 42
@@ -157,21 +157,21 @@ check_success "Modification du type de partition sur le disque N°1"
 pause
 
 # Ajout du disque N°1 dans la grappe RAID
-sudo mdadm --manage /dev/md1 --add $source_disk$source_part2
+mdadm --manage /dev/md1 --add $source_disk$source_part2
 check_success "Ajout de $source_disk$source_part2 à /dev/md1"
-sudo mdadm --manage /dev/md0 --add $source_disk$source_part1
+mdadm --manage /dev/md0 --add $source_disk$source_part1
 check_success "Ajout de $source_disk$source_part1 à /dev/md0"
 pause
 
 # Mise à jour de la configuration de MDADM
-sudo nano /etc/mdadm/mdadm.conf
+nano /etc/mdadm/mdadm.conf
 echo "Ajoutez DEVICE $source_disk$source_part1 $source_disk$source_part2 $target_disk$target_part1 $target_disk$target_part2 à mdadm.conf, puis appuyez sur Entrée pour continuer..."
 read
 
 # Mise à jour de GRUB et Initramfs sur le disque N°1
-sudo update-initramfs -u
+update-initramfs -u
 check_success "Mise à jour d'initramfs sur le disque N°1"
-sudo update-grub
+update-grub
 check_success "Mise à jour de GRUB sur le disque N°1"
 pause
 
