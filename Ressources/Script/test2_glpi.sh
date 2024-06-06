6f7f6c71cab36c65c0e60f4b0ec2fb1f77c480c7
Kamalors <162970946+Kamalors@users.noreply.github.com>
2024-06-06 10:52:10 +0200

    Update and rename test1_glpi.sh to test2_glpi.sh

--- Ressources/Script/test2_glpi.sh
+++ Ressources/Script/test2_glpi.sh
@@ -11,6 +11,9 @@ echo "Nom d'utilisateur: $db_user"
 echo "Mot de passe: $db_pass"
 echo "Nom de BDD: $db_name"
 
+# Ajout de l'entrée dans le fichier /etc/hosts pour résoudre pharmgreen.org
+echo "127.0.0.1 pharmgreen.org" >> /etc/hosts
+
 # Installation des paquets nécessaires
 apt update && apt upgrade -y
 apt install php-xml php-common php-json php-mysql php-mbstring php-curl php-gd php-intl php-zip php-bz2 php-imap php-apcu -y
