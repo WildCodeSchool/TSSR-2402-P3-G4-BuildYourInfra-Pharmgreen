1b148119180729b77dbcb7cf444fe7cf6a72480f
Kamalors <162970946+Kamalors@users.noreply.github.com>
2024-06-13 10:17:13 +0200

    Update Utilisateur_auto.ps1

--- Ressources/Script/Utilisateur_auto.ps1
+++ Ressources/Script/Utilisateur_auto.ps1
@@ -6,19 +6,21 @@
 
 $FilePath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 
-### Fichier a récupérer
-$File = "$FilePath\s09_Pharmgreen.csv"
+### Fichier à récupérer
+$File = "$FilePath\\s09_Pharmgreen.csv"
 
-### Initialisaiton
+### Initialisation
 
 If (-not(Get-Module -Name activedirectory))
 {
     Import-Module activedirectory
 }
-# Imporation des données
+# Importation des données
 $Users = Import-Csv -Path $File -Delimiter "," -Header "Prenom", "Nom", "Societe", "Site", "Departement", "Service", "fonction", "ManagerPrenom", "ManagerNom", "PC", "DateDeNaissance", ` "Telf", "Telp", "Nomadisme - Télétravail" | Select-Object -Skip 1
 $ADUsers = Get-ADUser -Filter * -Properties *
+
 $Count = 1
+
 Foreach ($User in $Users)
 {
     Write-Progress -Activity "Création des utilisateurs dans les OU" -Status "% effectué" -PercentComplete ($Count/$Users.Length*100)
@@ -42,45 +44,44 @@ Foreach ($User in $Users)
     $Site              = $User.Site
     $birthday          = $User.DateDeNaissance
 
-       # Gestion de présence de Sous OU
-        if ( $User.Service -eq "NA" )
-        # Chemin complet
-        { 
-                $Path = "ou=$($User.Departement),ou=User_Pharmgreen,dc=pharmgreen,dc=org"
-        }
-        Else
-        # Chemin sans sous OU
-        { 
-                $Path = "ou=$($User.Service),ou=$($User.Departement),ou=User_Pharmgreen,dc=pharmgreen,dc=org"      
-        }
-
-        # Création Utilisateur
-    If (($ADUsers | Where {$_.SamAccountName -eq $SamAccountName}) -eq $Null)
+    # Gestion de présence de Sous OU
+    if ( $User.Service -eq "NA" )
+    # Chemin complet
     {
-        Try 
+        $Path = "ou=$($User.Departement),ou=User_Pharmgreen,dc=pharmgreen,dc=org"
+    }
+    Else
+    # Chemin sans sous OU
+    {
+        $Path = "ou=$($User.Service),ou=$($User.Departement),ou=User_Pharmgreen,dc=pharmgreen,dc=org"
+    }
+
+    # Vérifier si l'utilisateur existe déjà
+    $existingUser = $ADUsers | Where-Object { $_.SamAccountName -eq $SamAccountName -and $_.Description -eq $birthday }
+
+    # Création Utilisateur
+    If ($existingUser -eq $Null)
+    {
+        Try
         {
             New-ADUser -Name $Name -DisplayName $DisplayName -SamAccountName $SamAccountName -UserPrincipalName $UserPrincipalName `
             -GivenName $GivenName -Surname $Surname -HomePhone $OfficePhone -MobilePhone $PortablePhone -EmailAddress $EmailAddress `
             -Office $Site -Description $birthday -Title $Fonction -City $Site -Path $Path `
             -AccountPassword (ConvertTo-SecureString -AsPlainText Azerty12024* -Force) -Enabled $True `
             -OtherAttributes @{Company = $Company;Department = $Department} -ChangePasswordAtLogon $True
-            
+
             Write-Host "Création du USER" $SamAccountName -ForegroundColor Green
         }
-        Catch  
+        Catch
         {
             write-host "Création du USER" $SamAccountName  "échoué" -ForegroundColor red
-
         }
     }
     Else
     {
-        Write-Host "Le USER $SamAccountName existe déjà" -ForegroundColor Yellow 
+        Write-Host "Le USER $SamAccountName existe déjà" -ForegroundColor Yellow
     }
-     
+
     $Count++
     sleep -Milliseconds 100
-
 }
-
-
