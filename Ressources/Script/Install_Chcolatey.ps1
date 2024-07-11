42c874a65c65488b08b79931d3cf0e6ff1ec0433
Bruno Serna <159529274+Mr-Maglor@users.noreply.github.com>
2024-07-11 15:09:26 +0200

    Create Install_Chcolatey.ps1

---
+++ Ressources/Script/Install_Chcolatey.ps1
@@ -0,0 +1,5 @@
+
+### Script pour installer automatiquement Chocolatey et faciliter l'installation des sondes Prometheus pour supervision Grafana.
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
